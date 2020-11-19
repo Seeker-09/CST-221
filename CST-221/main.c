c4a9c7ae21a706ad8467c751ec3df11e4c1879ee
Seeker-09 <creek.mundt2@gmail.com>
2020-11-19 01:29:25 -0500

    Initial Commit

---
+++ CST-221/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CST-221
+//
+//  Created by David Mundt on 11/19/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
